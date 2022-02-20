52db34913e6f0d7cd844bdffbb62bf23f493ec0f
Ryan Barr <ryanbarr@gmail.com>
2022-02-19 23:35:51 -0700

    Basic theming

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, LinkProps } from "react-router-dom";
+
+const NavigationLink = ({ to, children }: LinkProps) => {
+  return (
+    <Link
+      to={to}
+      className="text-white px-6 py-6 inline-block w-full hover:bg-indigo-700"
+    >
+      {children}
+    </Link>
+  );
+};
 
 const Navigation = () => {
   return (
-    <div>
+    <div className="bg-indigo-900 h-full">
       <ul>
         <li>
-          <Link to="/" className="text-blue-700">
-            Home
-          </Link>
+          <NavigationLink to="/">Home</NavigationLink>
         </li>
         <li>
-          <Link to="/test" className="text-blue-700">
-            Test
-          </Link>
+          <NavigationLink to="/test">Test</NavigationLink>
         </li>
       </ul>
     </div>
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,9 +3,11 @@ import Navigation from "../Navigation";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <Navigation />
-      {children}
+    <div className="flex w-full h-screen">
+      <div className="max-w-sm w-full flex-shrink">
+        <Navigation />
+      </div>
+      <div className="flex-grow">{children}</div>
     </div>
   );
 };
