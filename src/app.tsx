d291f287482d00c6b6fd69913d71239b696135fa
Joe Gallegos <32425596+joegallegos@users.noreply.github.com>
2022-02-19 22:40:03 -0700

    fix tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <><h2 className="text-red-500 text-8xl">Hello from React!</h2></>;
-}
+  return (
+    <>
+      <h2 className="text-blue-300 text-8xl">Hello from React!</h2>
+    </>
+  );
+};
 
 export default App;
