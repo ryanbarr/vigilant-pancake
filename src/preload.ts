93c07399d2296e2634ce356abd705b3086968e5e
Ryan Barr <ryanbarr@gmail.com>
2022-02-21 21:04:12 -0700

    Remove consoles from our preload.

--- src/preload.ts
+++ src/preload.ts
@@ -1,8 +1,5 @@
 import { contextBridge } from "electron";
 
-console.log("bananas");
-console.log(process.env.TWITCH_CLIENT_ID);
-
 contextBridge.exposeInMainWorld("envVars", {
   TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
 });
